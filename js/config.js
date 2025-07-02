5d5f8d0f16ae92a96d01750da1786052b51e0b2c
Mojahidmohd <mojahidmohd@gmail.com>
2025-07-02 08:59:56 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'me-south-1_ks3xR2NIl', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2pg499jo7chkbfid3k339g9te5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'me-south-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://w37gnr51u6.execute-api.me-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
